0111adad981c9c0d813477cd523dfbc9accbcc31
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-11 00:17:27 +0000

    added getUrlParameter to helper file

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -16,3 +16,10 @@ const getCookie = (name) => {
 const redirectTo = (url) => {
     window.location.href = url;
 }
+
+const getUrlParameter = (name) => {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
